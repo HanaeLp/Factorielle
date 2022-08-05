0944a1cc536ae89cca08c60a9bbaf251f83499f4
Hanae Zouiti <110188631+HanaeLp@users.noreply.github.com>
2022-08-05 17:56:34 +0100

    third commit

--- main.c
+++ main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int A,i,B=1;
-    printf("Donner un nombre :\n");
-    scanf("%d", &A);
+void fac(int a){
+    int i,B=1;
 
-    for(i = 1; i<=A; i++){
+    for(i = 1; i<=a; i++){
         B = B*i;
     }
-    printf("%d ! = %d", A, B );
+    printf(" par la fonction : %d ! = %d\n", a, B );
+}
 
+int main()
+{
+    int A,K,i,B=1;
+    printf("Donner un nombre :\n");
+    scanf("%d", &A);
+    fac(A);
+    K=A;
+    for(i = 1; i<=K; i++){
+        B = B*i;
+    }
+    printf(" par la methode simple : %d ! = %d", K, B );
 }
