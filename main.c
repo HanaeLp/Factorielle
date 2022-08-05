88494a59ff68f82f2cd567735e453ab0bdced8d7
Hanae Zouiti <110188631+HanaeLp@users.noreply.github.com>
2022-08-05 17:35:27 +0100

    second commit

--- main.c
+++ main.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fac(int a){
-    int i,B=1;
-
-    for(i = 1; i<=a; i++){
-        B = B*i;
-    }
-    printf("%d ! = %d", a, B );
-}
-
 int main()
 {
-    int A;
+    int A,i,B=1;
     printf("Donner un nombre :\n");
     scanf("%d", &A);
-    fac(A);
+
+    for(i = 1; i<=A; i++){
+        B = B*i;
+    }
+    printf("%d ! = %d", A, B );
+
 }
